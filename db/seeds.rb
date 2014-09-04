e756f9c807c822ad86852a3293685c868611065f
Tomwey <tomwey@163.com>
2014-09-05 01:09:52 +0800

    xxx

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
-# College.create(name: "西华师范大学")
-# College.create(name: "川北医学院")
+College.create(name: "西华师范大学")
+College.create(name: "川北医学院")
 College.create(name: "四川大学")
 College.create(name: "电子科技大学")
 
